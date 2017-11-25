2e923c59881f2fdd0aba28e8d6c881061ffd120e
Arandi Lopez <arandilopez.93@gmail.com>
2017-11-25 08:14:38 -0600

    node vendors y vue

--- public/js/ordenes_compra/form_orden_compra.js
+++ public/js/ordenes_compra/form_orden_compra.js
@@ -1,8 +1,48 @@
+// siempre debes usar esto
 (function () {
-  $(document).ready(function () {
-    $('#producto_select').on('change', function (e) {
-      let precio = $(this).find(':selected').data('precio');
-      $('#precio_unitario').val( precio );
-    });
+  new Vue({
+    el: '#crear_orden_compra_form',
+    data: {
+      cliente: '',
+      form_producto: {
+        producto_id: null,
+        precio_unitario: 0,
+        unidades: 1,
+        descripcion: ''
+        // importe: 0
+      },
+      productos: []
+    },
+    computed: {
+      importe: function () {
+        return (this.form_producto.unidades * this.form_producto.precio_unitario);
+      }
+    },
+    methods: {
+      setPrecioUnitario: function (event) {
+        let precio = $(event.target).find(':selected').data('precio');
+        this.form_producto.precio_unitario = precio;
+      },
+
+      agregarProducto: function () {
+        let producto = _.clone(this.form_producto);
+        if (producto.producto_id != null) {
+          this.productos.push(producto);
+          this.form_producto = {
+            producto_id: null,
+            precio_unitario: 0,
+            unidades: 1,
+            descripcion: ''
+            // importe: 0
+          };
+        }
+      }
+    }
   });
+  // $(document).ready(function () {
+  //   $('#producto_select').on('change', function (e) {
+  //     let precio = $(this).find(':selected').data('precio');
+  //     $('#precio_unitario').val( precio );
+  //   });
+  // });
 })();
--- resources/views/ordenes_compra/create.blade.php
+++ resources/views/ordenes_compra/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row" id="crear_orden_compra_form">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Crear nueva orden de compra</div>
@@ -17,12 +17,12 @@
                             </ul>
                         </div>
                     @endif
-                    <form class="form-horizontal" action="" method="post">
+                    <form class="form-horizontal">
                         <div class="row">
                             <div class="col-md-6 col-sm-6">
                                 <div class="form-group">
                                     <label for="cliente">Cliente</label>
-                                    <select class="form-control" name="cliente">
+                                    <select class="form-control" name="cliente" v-model="cliente">
                                         @foreach ($clientes as $cliente)
                                             <option value="{{$cliente->id}}">{{$cliente->nombre}}</option>
                                         @endforeach
@@ -36,7 +36,8 @@
                             <div class="col-md-6 col-sm-6">
                                 <div class="form-group">
                                     <label for="producto_select">Producto</label>
-                                    <select class="form-control" name="producto" id="producto_select">
+                                    <select class="form-control" name="producto" id="producto_select" v-on:change="setPrecioUnitario" v-model="form_producto.producto_id">
+                                        <option value="">-- Seleccione un producto --</option>
                                         @foreach ($productos as $producto)
                                             <option value="{{$producto->id}}" data-precio="{{$producto->precio}}">{{$producto->nombre}}</option>
                                         @endforeach
@@ -46,16 +47,60 @@
                                     <div class="col-md-6 col-sm-6">
                                         <div class="form-group">
                                             <label for="precio_unitario">P/U</label>
-                                            <input type="number" name="precio_unitario" value="{{old('precio_unitario')}}" class="form-control" id="precio_unitario">
+                                            <input type="number" name="precio_unitario" value="{{old('precio_unitario')}}" class="form-control" id="precio_unitario"
+                                            v-model="form_producto.precio_unitario">
                                         </div>
                                     </div>
                                     <div class="col-md-6 col-sm-6">
                                         <div class="form-group">
                                             <label for="unidades">Unidades</label>
-                                            <input type="number" name="unidades" value="{{old('unidades')}}" class="form-control">
+                                            <input type="number" name="unidades" value="{{old('unidades')}}" class="form-control" v-model="form_producto.unidades">
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4">
+                                <div class="form-group">
+                                    <label>Importe</label>
+                                    <p class="form-control-static">
+                                        @{{ _.round(importe, 2) }}
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4">
+                                <div class="form-group">
+                                    <label for="">Descripcion</label>
+                                    <input type="text" name="descripcion" class="form-control" v-model="form_producto.descripcion">
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4">
+                                <button type="button" class="btn btn-success" v-on:click="agregarProducto">
+                                    Agregar
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Producto</th>
+                                            <th>P/U</th>
+                                            <th>Unidades</th>
+                                            <th>Importe</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="producto in productos">
+                                            <td>@{{ producto.descripcion }}</td>
+                                            <td>@{{ producto.precio_unitario }}</td>
+                                            <td>@{{ producto.unidades }}</td>
+                                            <td>@{{ (producto.unidades * producto.precio_unitario) }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </form>
